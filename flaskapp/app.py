b75d38058ae7905ce2813f3ffb0d2bfcdb727f6f
unknown <markhcrawford@yahoo.com>
2023-08-21 21:49:00 -0400

    4th com

--- flaskapp/app.py
+++ flaskapp/app.py
@@ -48,4 +48,4 @@ def search():
     return render_template("index.html", query = query, selected_field=selected_field, results = results)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
